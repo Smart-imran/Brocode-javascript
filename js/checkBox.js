a84a3732c2382b7190593f5878bcd7ad1c821e91
ict-imran <ict.imaran@hotmail.com>
2024-06-22 00:52:30 +0600

    8th checkbox

---
+++ js/checkBox.js
@@ -0,0 +1,10 @@
+
+document.getElementById("mybtn").onclick = function(){
+
+    if (document.getElementById("myCheckbox").checked) {
+        console.log("You are subscribe");
+    }
+    else{
+        console.log("You are not subscribe");
+    }
+}
